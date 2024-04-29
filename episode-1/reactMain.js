f3f88fe893ea6de12f1b59aafc3e86340dfb23b8
@rathodsushant9 <rathodsushant@gmail.com>
2024-04-30 01:14:28 +0530

    complicated element is created

--- episode-1/reactMain.js
+++ episode-1/reactMain.js
@@ -14,15 +14,48 @@
  *
  */
 
-let element = React.createElement(
-  "div",
-  { id: "parent" },
+// let element = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("h1", {}, "First element")
+//   )
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(element);
+
+/**
+ *
+ * <div id="parent">
+ * <div id="child-1">
+ * <h1>
+ * <\h1>
+ * <\div>
+ * <div id="child-2">
+ * <h1>
+ * <\h1>
+ * * <h1>
+ * <\h1>
+ * <\div>
+ * <\div
+ *
+ */
+
+const element2 = React.createElement("div", { id: "parent" }, [
   React.createElement(
     "div",
-    { id: "child" },
-    React.createElement("h1", {}, "First element")
-  )
-);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
+    { id: "child-1" },
+    React.createElement("h1", {}, "Child Div 1 h1 ")
+  ),
+  React.createElement("div", { id: "child-2" }, [
+    React.createElement("h1", {}, "Child Div 2 h1"),
+    React.createElement("h1", {}, "child Div2 h1"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(element2);
+ // all inside root will be replaced by element2 rest all are as it is.
+ complicated
